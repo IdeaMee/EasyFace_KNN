34a1ccf7019a0e9a11b634f3b3d1836939841615
Kanawatkha <kanawatkha@gmail.com>
2026-09-25 12:00:10 +0700

    Add None (no-face) training data and update FaceReg with recognition loop

--- FaceReg.py
+++ FaceReg.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 import os
 
-def knn(X, y, z, k=1):
+def knn(X, y, z, k=5):
     d = np.sum((X - z) ** 2, axis=1)
     idx = np.argsort(d)[:k]
     cls, vote = np.unique(y[idx], return_counts=True)
@@ -18,5 +19,17 @@ for f in os.listdir():
                 X.append(x.flatten())
                 y.append(f)
 X = np.array(X)
+y = np.array(y)
 print(X.shape)
 print(y)
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+while True:
+    ret, frame = cap.read()
+    cv2.rectangle(frame, (250,120), (390,300), (0,0,255), 2)
+    face = cv2.cvtColor(frame[120:300, 250:390, :], cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', frame)
+    z = face.flatten()
+    name = knn(X, y, z)
+    print(name)
+    cv2.waitKey(1)
